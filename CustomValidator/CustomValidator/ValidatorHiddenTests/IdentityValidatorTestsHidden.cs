29e9e48315391a7910d69e4fb5d6a3f244f7afc1
alenaivarouskaya <45531342+alenaivarouskaya@users.noreply.github.com>
2018-12-11 10:50:06 +0300

    Add files via upload

---
+++ CustomValidator/CustomValidator/ValidatorHiddenTests/IdentityValidatorTestsHidden.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+namespace CustomValidator
+{
+    [TestFixture]
+    public class IdentityValidatorTests
+    {
+        [Test]
+        public void NameShoudNotBeNull_ValidateTest()
+        {
+            var validator = new IdentityValidator();
+            var expected = true;
+            var actual = validator.Validate(null);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void NameShoudNotBeShorter9Symbols_ValidateTest()
+        {
+            var validator = new IdentityValidator();
+            var expected = true;
+            var actual = validator.Validate("shortname");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void NameShoudNotBeEmptyString_ValidateTest()
+        {
+            var validator = new IdentityValidator();
+            var expected = true;
+            var actual = validator.Validate(string.Empty);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# CustomValidator
+Custom Validation Task for NET Padawans
+
+Please fork this repository.
+1)Implement methods of CustomValidator class by needed modifications and functionality.<br/>
+2)Check Build and Run sample Unit tests.<br/>
+3) Add your own Unit tests for all possible usage scenarious.  <br/>
+4) Submit your solution using Padawans application.<br/>
+5) See your Build and tests results.<br/>
+6) Note, that you may Submit your solution not more than 2 times for every task<br/>
+Good luck and Happy coding!
